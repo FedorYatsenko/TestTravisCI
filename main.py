d1b92aec989cf6662563524d357ac646c71f0d91
FedirYatsenko <feda159357@gmail.com>
2023-03-06 12:08:21 +0200

    Init

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+def my_add(a: float, b: float) -> float:
+    """Function to add float numbers"""
+    return a + b
+
+
+def main() -> None:
+    res = my_add('a', 'b')
+
+    print('res =', res)
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_main.py
@@ -0,0 +1,6 @@
+import main
+
+
+def test_my_add():
+    """Testing my_add function"""
+    assert main.my_add(1, 2) == 3
